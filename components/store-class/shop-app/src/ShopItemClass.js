fb1fc2f0915ab437fe3ddfc04976e4585f47768b
Natalia Svetlitskaya <svetlitskaya73@gmail.com>
2025-06-24 15:09:02 +0300

    components-second-task

---
+++ components/store-class/shop-app/src/ShopItemClass.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class ShopItemClass extends Component {
+  render() {
+    const { brand, title, description, descriptionFull, price, currency } = this.props.item;
+    const formattedPrice = `${currency}${price.toFixed(2)}`;
+
+    return (
+      <div className="main-content">
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
+        <div className="description">{descriptionFull}</div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{formattedPrice}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShopItemClass;
